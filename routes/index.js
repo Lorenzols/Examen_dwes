b9b10f208ba7d4636988b157e6450c0d0724c309
lorenzo <lorenzolsbit@gmail.com>
2022-02-22 14:39:59 +0100

    api rick ok

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const passport = require('passport')
 const Coches = require('../models/coches')
+const request = require('request')
 
 const {logged, Notlogged} = require('../lib/protect')
 
@@ -43,5 +44,17 @@ router.get('/api', async(req, res, next)  => {
   res.json({coches})
 });
 
+
+
+router.get('/rick', async(req, res, next)  => {
+  request(`https://rickandmortyapi.com/api/character`, {json: true}, (err, response ,body) => { 
+    if(err) {
+      return console.log(err)
+    }
+    console.log(body.results[0])
+    const datos = body.results
+    res.render('api/rick', {datos})
+  })
+});
 
 module.exports = router;
